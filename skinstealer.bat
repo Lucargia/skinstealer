e1fda85137a6f22583c565fadf0b56397048f85c
Lucargia <Lucargia@hotmail.com>
2013-08-23 09:12:49 -0700

    Removed unwanted code

--- skinstealer.bat
+++ skinstealer.bat
@@ -4,7 +4,6 @@ echo Please enter the username for the skin you want to steal:
 set /p userskin=
 pause
 echo Found skin
-else goto nf
 start http://www.minecraft.net/skin/%userskin%.png
 pause
 exit
